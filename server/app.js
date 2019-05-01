b63b26ffd64aab6f8906877b953b06244e8f3af1
Sam <samryoo@gmail.com>
2019-05-01 16:38:14 -0700

    Revert "merged data with backend"

--- server/app.js
+++ server/app.js
@@ -3,12 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const PORT = 3000;
+
 const app = express();
-const server = require('http').createServer(app)
-
-const io = require('socket.io')(server);
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -21,8 +17,6 @@ app.get('/', (req, res)=>{
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cookieParser())
@@ -31,38 +25,10 @@ app.use(cookieParser())
 // app.use('/build', express.static(path.join(__dirname, '../build')));
 app.use('/api', productRoute);
 
-// app.use('/', (req, res) => {
-  //   res.sendFile(path.join(__dirname, '../index.html'));
-  //   res.send('reached root route');
-  // });
-  
-  
-  
-  
-  
-  //EVERYTHING BELOW IS SOCKET.IO RELATED
-  
-  app.get('/chat',function(req,res) {
-    res.sendFile(path.resolve(__dirname + '/../indexChat.html'))
-  })
-  
-  
-  io.on('connection', function (socket){
-
-
-    socket.on('chat message', function(msg){
-      io.emit('chat message', msg)
-      
-    })
-    
-  })
-  
-  
-  server.listen(PORT, '192.168.0.59', () => {
-    console.log(`Listening on ${PORT}.`)
-  })
-
-
+app.use('/', (req, res) => {
+ // res.sendFile(path.join(__dirname, '../index.html'));
+  res.send('reached root route');
+});
 
 
 module.exports = app;
