0b270e3a570d2a5a8b1f552c61bb8439d23eee71
j218 <jaymlim93@gmail.com>
2019-05-01 12:30:09 -0700

    working on testing

--- client/components/home.js
+++ client/components/home.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 
 const Home = () => (
   <div>
-    <h1>Buy a keyboard wrist rest that perfectly compliments your style</h1>
+    <h1><strong>Buy a keyboard wrist rest that perfectly compliments your style</strong></h1>
     <Link to="/wood">get started </Link>
   </div>
 );
--- server/app.js
+++ server/app.js
@@ -53,8 +53,7 @@ server.listen(PORT, () => {
 
 io.on('connection', function (socket){
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg)
-    
+    io.sockets.emit('chat message', msg)
   })
 
 })
